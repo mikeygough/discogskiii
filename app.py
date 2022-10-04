fc7084d0e32b8871d32b0004b0779e448b395646
mikey <mikey@hey.com>
2022-10-04 13:26:49 -0500

    add flask & sample app

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from config import *
+
+from flask import Flask, render_template, request
 import json
 import requests
 
-r = requests.get('https://api.discogs.com/releases/249504', auth=(CONSUMER_KEY, CONSUMER_SECRET))
+# turn this file into a flask application
+app = Flask(__name__)
 
-# sample response
-print("Status Code: ", r.status_code)
-print("Headers Content Type: ", r.headers['content-type'])
-# clean json
-parsed_r = json.loads(r.text)
-print(json.dumps(parsed_r, indent=4))
+@app.route("/")
+def index():
+    r = requests.get('https://api.discogs.com/releases/249504', auth=(CONSUMER_KEY, CONSUMER_SECRET))
+
+    # sample response
+    # print("Status Code: ", r.status_code)
+    # print("Headers Content Type: ", r.headers['content-type'])
+    # clean json
+    parsed_r = json.loads(r.text)
+    # print(json.dumps(parsed_r, indent=4))    
+    
+    return render_template("index.html", parsed_r=parsed_r)
+
+
+
